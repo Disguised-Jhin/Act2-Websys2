d835ae07057acf1532b5292e327f9f087e0b1444
Disguised-Jhin <20223752@s.ubaguio.edu>
2025-09-05 19:10:54 +0800

    Update controllers.js

--- src/controllers.js
+++ src/controllers.js
@@ -1,19 +1,27 @@
-function filterName(data, nameQuery) {
+function filtName(data, nameQuery) {
     return data.filter(product => product.name.toLowerCase().includes(nameQuery.toLowerCase()))
 }
 
-function filterDescription(data, descriptionQuery) {
+function filtDescription(data, descriptionQuery) {
     return data.filter(product => product.desc.toLowerCase().includes(descriptionQuery.toLowerCase()))
 }
 
-function filterPriceRange(data, lowerValue, upperValue) {
+function filtPriceRange(data, lowerValue, upperValue) {
     return data.filter(product => product.price >= lowerValue && product.price <= upperValue)
 }
 
-function filterStockRange(data, lowerValue, upperValue) {
+function filtStockRange(data, lowerValue, upperValue) {
     return data.filter(product => product.stock >= lowerValue && product.stock <= upperValue)
 }
 
+function filtStatus(data, status){
+    return data.filter(order => order.status = status)
+}
+
+function filtTotalAmount(data, total) {
+    return data.filter(order => order.total)
+}
+
 function filterStatus(data, status){
     return data.filter(order => order.status = status)
 }
@@ -23,23 +31,12 @@ function filterTotalAmount(data, total) {
 }
 
 module.exports = {
-    filterName, 
-    filterDescription,
-    filterPriceRange,
-    filterStockRange,
-    filterStatus,
-    filterTotalAmount
-}
-
-function filterStatus(data, status){
-    return data.filter(order => order.status = status)
-}
-
-function filterTotalAmount(data, total) {
-    return data.filter(order => order.total)
-}
-
-module.exports = {
+    filtName, 
+    filtDescription,
+    filtPriceRange,
+    filtStockRange,
+    filtStatus,
+    filtTotalAmount,
     filterStatus,
     filterTotalAmount
 }
