02c36c7278a4a2766b5b664f6f7c8ea5bd120cd3
Disguised-Jhin <20223752@s.ubaguio.edu>
2025-09-05 19:06:28 +0800

    Create controllers.js

---
+++ src/controllers.js
@@ -0,0 +1,45 @@
+function filterName(data, nameQuery) {
+    return data.filter(product => product.name.toLowerCase().includes(nameQuery.toLowerCase()))
+}
+
+function filterDescription(data, descriptionQuery) {
+    return data.filter(product => product.desc.toLowerCase().includes(descriptionQuery.toLowerCase()))
+}
+
+function filterPriceRange(data, lowerValue, upperValue) {
+    return data.filter(product => product.price >= lowerValue && product.price <= upperValue)
+}
+
+function filterStockRange(data, lowerValue, upperValue) {
+    return data.filter(product => product.stock >= lowerValue && product.stock <= upperValue)
+}
+
+function filterStatus(data, status){
+    return data.filter(order => order.status = status)
+}
+
+function filterTotalAmount(data, total) {
+    return data.filter(order => order.total)
+}
+
+module.exports = {
+    filterName, 
+    filterDescription,
+    filterPriceRange,
+    filterStockRange,
+    filterStatus,
+    filterTotalAmount
+}
+
+function filterStatus(data, status){
+    return data.filter(order => order.status = status)
+}
+
+function filterTotalAmount(data, total) {
+    return data.filter(order => order.total)
+}
+
+module.exports = {
+    filterStatus,
+    filterTotalAmount
+}
